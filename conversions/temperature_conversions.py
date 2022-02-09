735a9354e2699017aa7724de3b10a0c2f131da32
Noonuu <astrophysicsandpython@gmail.com>
2022-02-10 00:50:21 +0500

    Added conversion_error_base.py, temperature_conversions.py, conversion/__init__.py, and black_body/__init__.py and bunch of other stuff

---
+++ conversions/temperature_conversions.py
@@ -0,0 +1,101 @@
+"""
+Created on Feb 09 23:38:17 2022
+"""
+
+import conversion_error_base as ceb
+
+
+class FromCelsius:
+
+    def __init__(self, temperature: float, convert_to: str = 'F') -> None:
+        self.temperature = temperature
+        self.convert_to = convert_to
+
+    def _to_kelvin(self):
+        return self.temperature + 273.15
+
+    def _to_fahrenheit(self):
+        return (self.temperature * 1.8) + 32  # 9/5 = 1.8
+
+    def _to_rankine(self):
+        return (self.temperature * 1.8) + 491.67
+
+    def convert(self):
+        sep, convertible, _c = ', ', ['f', 'k', 'r', 'fahrenheit', 'kelvin', 'rankine'], self.convert_to.lower()
+
+        if _c not in convertible:
+            ceb.raise_error(separator=sep, units=convertible)
+
+        return self._to_fahrenheit() if _c in ['f', 'fahrenheit'] else self._to_kelvin() if _c in ['k', 'kelvin'] else self._to_rankine()
+
+
+class FromKelvin:
+
+    def __init__(self, temperature: float, convert_to: str = 'C'):
+        self.temperature = temperature
+        self.convert_to = convert_to
+
+    def _to_celsius(self):
+        return self.temperature - 273.15
+
+    def _to_fahrenheit(self):
+        return (self._to_celsius() * 1.8) + 32
+
+    def _to_rankine(self):
+        return self.temperature * 1.8
+
+    def convert(self):
+        sep, convertible, _c = ', ', ['c', 'f', 'r', 'celsius', 'fahrenheit', 'rankine'], self.convert_to.lower()
+
+        if _c not in convertible:
+            ceb.raise_error(separator=sep, units=convertible)
+
+        return self._to_celsius() if _c in ['c', 'celsius'] else self._to_fahrenheit() if _c in ['f', 'fahrenheit'] else self._to_rankine()
+
+
+class FromFahrenheit:
+
+    def __init__(self, temperature: float, convert_to: str = 'C'):
+        self.temperature = temperature
+        self.convert_to = convert_to
+
+    def _to_celsius(self):
+        return (self.temperature - 32) * 1.8
+
+    def _to_kelvin(self):
+        return self._to_celsius() + 273.15
+
+    def _to_rankine(self):
+        return self.temperature + 459.67
+
+    def convert(self):
+        sep, convertible, _c = ', ', ['c', 'k', 'r', 'celsius', 'kelvin', 'rankine'], self.convert_to.lower()
+
+        if _c not in convertible:
+            ceb.raise_error(separator=sep, units=convertible)
+
+        return self._to_celsius() if _c in ['c', 'celsius'] else self._to_kelvin() if _c in ['k', 'kelvin'] else self._to_rankine()
+
+
+class FromRankine:
+
+    def __init__(self, temperature: float, convert_to: str = 'C'):
+        self.temperature = temperature
+        self.convert_to = convert_to
+
+    def _to_fahrenheit(self):
+        return self.temperature - 459.67
+
+    def _to_celsius(self):
+        return (self.temperature - 491.67) * 1.8
+
+    def _to_kelvin(self):
+        return self.temperature / 1.8
+
+    def convert(self):
+        sep, convertible, _c = ', ', ['c', 'k', 'f', 'celsius', 'kelvin', 'fahrenheit'], self.convert_to.lower()
+
+        if _c not in convertible:
+            ceb.raise_error(separator=sep, units=convertible)
+
+        return self._to_celsius() if _c in ['c', 'celsius'] else self._to_kelvin() if _c in ['k', 'kelvin'] else self._to_fahrenheit()
