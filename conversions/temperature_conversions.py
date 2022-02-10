202de8202e4d2608ace8e7d4c7832b3ae7f1eb4a
Noonuu <astrophysicsandpython@gmail.com>
2022-02-11 00:58:08 +0500

    Added docstring to classes and __init__ functions

--- conversions/temperature_conversions.py
+++ conversions/temperature_conversions.py
@@ -6,8 +6,51 @@ import conversion_error_base as ceb
 
 
 class FromCelsius:
+    """Conversion class from degree Celsius to one of Kelvin, degree Fahrenheit, or Rankine temperature scale.
+
+    FUNCTIONS
+    ----------
+    __init__(temperature: float, convert_to: str = 'F')
+        Class initializer function
+
+    convert()
+        Convert function to convert degree Celsius temperature.
+
+    Examples
+    ----------
+    Convert 100 degree Celsius temperature to Kelvin, degree Fahrenheit, and Rankine scale.
+
+    >>> FromCelsius(100, 'F').convert()
+
+    >>> FromCelsius(100, 'Kelvin').convert()
+
+    >>> FromCelsius(100, 'rankine').convert()
+    """
 
     def __init__(self, temperature: float, convert_to: str = 'F') -> None:
+        """
+
+        Parameters
+        ----------
+        temperature :
+            Measured temperature.
+        convert_to :
+            Scale indicator, which temperature scale you want to convert to.
+
+        Returns
+        -------
+        object:
+            Converted temperature from degree Celsius.
+
+        Notes
+        -------
+            convert_to variable takes either
+
+            - K, Kelvin
+            - F, degree Fahrenheit
+            - R, Rankine
+            as valid inputs. However, the string literal check in case-insensitive.
+        """
         self.temperature = temperature
         self.convert_to = convert_to
 
@@ -33,8 +76,51 @@ class FromCelsius:
 
 
 class FromKelvin:
+    """Conversion class from Kelvin to one of degree Celsius, degree Fahrenheit, or Rankine temperature scale.
+
+    FUNCTIONS
+    ----------
+    __init__(temperature: float, convert_to: str = 'C')
+        Class initializer function
+
+    convert()
+        Convert function to convert Kelvin temperature.
+
+    Examples
+    ----------
+    Convert 100 Kelvin temperature to degree Celsius, degree Fahrenheit, and Rankine scale.
+
+    >>> FromKelvin(100, 'C').convert()
+
+    >>> FromKelvin(100, 'f').convert()
+
+    >>> FromKelvin(100, 'rankine').convert()
+    """
 
     def __init__(self, temperature: float, convert_to: str = 'C'):
+        """
+
+        Parameters
+        ----------
+        temperature :
+            Measured temperature.
+        convert_to :
+            Scale indicator, which temperature scale you want to convert to.
+
+        Returns
+        -------
+        object:
+            Converted temperature from Kelvin.
+
+        Notes
+        -------
+            convert_to variable takes either
+
+            - C, degree Celsius
+            - F, degree Fahrenheit
+            - R, Rankine
+            as valid inputs. However, the string literal check in case-insensitive.
+        """
         self.temperature = temperature
         self.convert_to = convert_to
 
@@ -60,8 +146,51 @@ class FromKelvin:
 
 
 class FromFahrenheit:
+    """Conversion class from degree Fahrenheit to one of degree Celsius, Kelvin, or Rankine temperature scale.
+
+    FUNCTIONS
+    ----------
+    __init__(temperature: float, convert_to: str = 'C')
+        Class initializer function
+
+    convert()
+        Convert function to convert degree Fahrenheit temperature.
+
+    Examples
+    ----------
+    Convert 100 degree Fahrenheit temperature to degree Celsius, Kelvin, and Rankine scale.
+
+    >>> FromFahrenheit(100, 'C').convert()
+
+    >>> FromFahrenheit(100, 'Kelvin').convert()
+
+    >>> FromFahrenheit(100, 'rankine').convert()
+    """
 
     def __init__(self, temperature: float, convert_to: str = 'C'):
+        """
+
+        Parameters
+        ----------
+        temperature :
+            Measured temperature.
+        convert_to :
+            Scale indicator, which temperature scale you want to convert to.
+
+        Returns
+        -------
+        object:
+            Converted temperature from degree Fahrenheit.
+
+        Notes
+        -------
+            convert_to variable takes either
+
+            - C, degree Celsius
+            - K, Kelvin
+            - R, Rankine
+            as valid inputs. However, the string literal check in case-insensitive.
+        """
         self.temperature = temperature
         self.convert_to = convert_to
 
@@ -87,8 +216,51 @@ class FromFahrenheit:
 
 
 class FromRankine:
+    """Conversion class from Rankine to one of degree Celsius, degree Fahrenheit, or Kelvin temperature scale.
+
+    FUNCTIONS
+    ----------
+    __init__(temperature: float, convert_to: str = 'C')
+        Class initializer function
+
+    convert()
+        Convert function to convert Rankine temperature.
+
+    Examples
+    ----------
+    Convert 100 Rankine temperature to degree Celsius, degree Fahrenheit, and Kelvin scale.
+
+    >>> FromRankine(100, 'C').convert()
+
+    >>> FromRankine(100, 'F').convert()
+
+    >>> FromRankine(100, 'Kevlin').convert()
+    """
 
     def __init__(self, temperature: float, convert_to: str = 'C'):
+        """
+
+        Parameters
+        ----------
+        temperature :
+            Measured temperature.
+        convert_to :
+            Scale indicator, which temperature scale you want to convert to.
+
+        Returns
+        -------
+        object:
+            Converted temperature from Rankine.
+
+        Notes
+        -------
+            convert_to variable takes either
+
+            - C, degree Celsius
+            - K, Kelvin
+            - F, degree Fahrenheit
+            as valid inputs. However, the string literal check in case-insensitive.
+        """
         self.temperature = temperature
         self.convert_to = convert_to
 
